faa5d0a5ff5cbb01f9203492b9e3dbddc7ebbb3c
jochen <jochen@netlog.com>
2020-02-05 09:50:36 +0100

    Fix for customTestingBotEnd : updateTest expects data first, then sessionId

--- custom_commands/customTestingBotEnd.js
+++ custom_commands/customTestingBotEnd.js
@@ -1,23 +1,23 @@
 exports.command = function(callback) {
-    var TestingBot = require("testingbot-api");
+	var TestingBot = require("testingbot-api");
 
-    var tb = new TestingBot({
-        api_key: process.env.TB_KEY,
-        api_secret: process.env.TB_SECRET
-    });
+	var tb = new TestingBot({
+		api_key: process.env.TB_KEY,
+		api_secret: process.env.TB_SECRET
+	});
 
-    var sessionid = this.capabilities['webdriver.remote.sessionid'],
-        jobName = this.currentTest.name,
-        passed = this.currentTest.results.testcases[jobName].failed === 0;
+	var sessionid = this.capabilities['webdriver.remote.sessionid'],
+		jobName = this.currentTest.name,
+		passed = this.currentTest.results.testcases[jobName].failed === 0;
 
 
-    console.log("TestingBotSessionId=" + sessionid);
+	console.log("TestingBotSessionId=" + sessionid);
 
 	var self = this
-	tb.updateTest(sessionid, {
-        'test[success]': passed,
-        'test[name]': jobName
-	}, function () {
-    	self.end(callback)
+	tb.updateTest({
+		'test[success]': passed,
+		'test[name]': jobName
+	}, sessionid, function () {
+		self.end(callback)
 	});
 };
